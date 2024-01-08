4e0e27541ff4a03eec36e0a5868a015170ea0d8a
Muthoni Njoki <fmuthoninjoki@gmail.com>
2024-01-08 23:29:52 +0300

    Added a homepage

--- mimis-ecommerce-site/src/App.jsx
+++ mimis-ecommerce-site/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Layout from './components/layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
-  return (
-    <>
-      
+  return <>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Layout />} ></Route>
+     </Routes>
+     </BrowserRouter>
     </>
-  )
 }
 
 export default App
--- mimis-ecommerce-site/src/components/layout.jsx
+++ mimis-ecommerce-site/src/components/layout.jsx
@@ -5,9 +5,9 @@ import { Outlet } from 'react-router-dom'
 
 const layout = () => {
   return <>
-      <Header></Header>
+      <Header />
       <Outlet />
-      <Footer></Footer>
+      <Footer />
     </>
 }
 
