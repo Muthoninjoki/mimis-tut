56147d6a370a1e9f2b30976d4a8f42e70e3eb4d2
Muthoni Njoki <fmuthoninjoki@gmail.com>
2024-01-21 23:24:47 +0300

    Added some styling and info to the footer

--- mimis-ecommerce-site/src/components/footer.jsx
+++ mimis-ecommerce-site/src/components/footer.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const footer = () => {
   return <>
-  <footer classname='footer'>
+  <footer classname='footer px-5'>
     <div className="container-xxl">
-      
+      <div className="row">
+        <div className="col-4 d-flex flex-column">
+          <h3 className='mb-5'>Contact Us</h3>
+          <div className="footer-details">
+            <p>Address:</p>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
   </>
