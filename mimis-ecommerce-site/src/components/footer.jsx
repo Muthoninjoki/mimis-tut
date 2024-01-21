08bbd78893ef21bce1b78fd7a74acf7dc0d32e17
Muthoni Njoki <fmuthoninjoki@gmail.com>
2024-01-21 23:32:01 +0300

    Added some styling and info to the footer

--- mimis-ecommerce-site/src/components/footer.jsx
+++ mimis-ecommerce-site/src/components/footer.jsx
@@ -8,7 +8,9 @@ const footer = () => {
         <div className="col-4 d-flex flex-column">
           <h3 className='mb-5'>Contact Us</h3>
           <div className="footer-details">
-            <p>Address:</p>
+            <p className='mb-3'><b>Address:</b> Milele Center 1st floor, Kitengela</p>
+            <p className='mb-3'><b>Phone:</b> <a href="tel:+25412345678">Call Us +25412345678</a></p>
+            <p className='mb-5'><b>Hours Open</b> We are open from 8:30am - 5:00pm</p>
           </div>
         </div>
       </div>
