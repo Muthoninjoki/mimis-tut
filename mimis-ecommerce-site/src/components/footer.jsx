8922f1c8273961d5ddfd48b56e8c367d982f9d96
Muthoni Njoki <fmuthoninjoki@gmail.com>
2024-01-23 23:51:49 +0300

    Added social links

--- mimis-ecommerce-site/src/components/footer.jsx
+++ mimis-ecommerce-site/src/components/footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AiFillTwitterCircle } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
+import { BiLogoFacebook } from 'react-icons/bi';
 
 const footer = () => {
   return <>
@@ -11,6 +14,11 @@ const footer = () => {
             <p className='mb-3'><b>Address:</b> Milele Center 1st floor, Kitengela</p>
             <p className='mb-3'><b>Phone:</b> <a href="tel:+25412345678">Call Us +25412345678</a></p>
             <p className='mb-5'><b>Hours Open</b> We are open from 8:30am - 5:00pm</p>
+            <p className='mb-3'><b>Follow Us</b></p>
+            <div className="social-icons justify-content-around">
+              <Link><AiFillTwitterCircle /></Link>
+
+            </div>
           </div>
         </div>
       </div>
